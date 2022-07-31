ad843f47ed8b52f8a845a2dfed674996487e0a39
nimikevin <97411659+nimikevin@users.noreply.github.com>
2022-07-31 19:17:16 -0400

    app barre

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: Column(
+          children: [
+            //app bar
 
-class HomePage extends StatelessWidget{
-  const HomePage ({ Key? key }) : super (key:key);
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    //mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 7),
+                      Text(
+                        'Kevin Nimi',
+                        style: TextStyle(
+                            fontSize: 23, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(8.00),
+                      decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(12)
+                      ),
+                      child: Icon((Icons.person)))
+                ],
+              ),
+            )
 
-  @override 
-  Widget build(BuildContext context){
-    return Scaffold();
+            //card
+
+            //barre de recherche
+
+            //categorie
+
+            //liste docteur
+          ],
+        ),
+      ),
+    );
   }
 }
