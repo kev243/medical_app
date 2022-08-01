b004f984fed41fb9fe4257fe17bab9c01e8b7929
nimikevin <97411659+nimikevin@users.noreply.github.com>
2022-07-31 22:31:59 -0400

    travail sur le card

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -37,15 +38,61 @@ class HomePage extends StatelessWidget {
                   Container(
                       padding: EdgeInsets.all(8.00),
                       decoration: BoxDecoration(
-                      color: Colors.deepPurple[100],
-                      borderRadius: BorderRadius.circular(12)
-                      ),
+                          color: Colors.deepPurple[100],
+                          borderRadius: BorderRadius.circular(12)),
                       child: Icon((Icons.person)))
                 ],
               ),
-            )
+            ),
 
-            //card
+            //card////////////////
+
+            SizedBox(height: 25),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.pink[100],
+                    borderRadius: BorderRadius.circular(12)),
+                child: Row(
+                  children: [
+                    //animation picture
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.deepPurple[300],
+                    ),
+                    SizedBox(width: 20),
+
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Comment vous sentez vous?',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16)),
+                          SizedBox(height: 12),
+                          Text('Remplissez votre carte médicale dès maintenant',
+                              style: TextStyle(fontSize: 13)),
+                          SizedBox(height: 12),
+                          Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple[200],
+                                borderRadius: BorderRadius.circular(12)),
+                            child: Center(child: Text('Commencer')),
+                          )
+                        ],
+                      ),
+                    ),
+
+                    //
+                  ],
+                ),
+              ),
+            )
 
             //barre de recherche
 
